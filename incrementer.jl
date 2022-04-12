875cc51330c84f3142d66998fbfa29f7ef6d3389
hugofolloni <hugofollogua07@gmail.com>
2022-04-12 17:20:28 -0300

    :fire: Adding a loopIncrementer

--- incrementer.jl
+++ incrementer.jl
@@ -1,4 +1,5 @@
 function increment(initial, incrementer, times)
+    println("\nSimple incrementer:")
     return println(initial + incrementer * times)
 end
 # Essa função deve incrementar ao número inicial passado por parâmetro o incremento x vezes.
@@ -7,3 +8,13 @@ increment(200, 15, 10)
 increment(100, 0.25, 100)
 
 # To run Julia file, type: julia> include("path\\to\\file.jl")
+
+function loopIncrementer(initial, increment, times)
+    println("\nLoop incrementer for $initial. Adding $increment for $times times:")
+    println(initial)
+    for i in 1:times
+        println(initial + increment * i)
+    end
+end
+
+loopIncrementer(200, 15, 10)
